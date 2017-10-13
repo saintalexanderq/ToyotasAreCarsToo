54b11cd84e2ac999ed0cb6986fda119dc290f265
saintalexanderq <xanderquigley@gmail.com>
2017-10-13 16:48:55 -0300

    Initial Commit

---
+++ Assignment3Toyota/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment3Toyota
+//
+//  Created by Alex Quigley on 2017-10-13.
+//  Copyright © 2017 Alex Quigley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
